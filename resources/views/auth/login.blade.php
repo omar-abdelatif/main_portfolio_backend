fcc796cd3c1d94813023bf130d4be8809b5af078
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-04-26 22:50:33 +0300

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" class="text-white form-labels" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full text-dark" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -19,7 +19,7 @@
         <div class="mt-4">
             <x-input-label for="password" class="text-white form-labels" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full text-dark" type="password" name="password" required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
