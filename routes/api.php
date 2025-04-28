97752690b168a4f226e033e4536b84b365ce606d
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-04-28 13:07:44 +0300

    update project details url

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\ApiController;
 Route::middleware('api.key')->prefix('api/v1')->group(function () {
     Route::controller(ApiController::class)->group(function () {
         Route::get('/projects', 'allProjects')->name('api.projects.all');
-        Route::get('/projects/{slug}', 'projectDetails')->name('api.projects.show');
+        Route::get('/projects/project_details/{slug}', 'projectDetails')->name('api.projects.show');
         Route::get('/pricing/plans', 'PricingPlan')->name('api.pricing.plans');
         Route::get('/social/links', 'socialLinks')->name('api.social.links');
         Route::get('/payment/methods', 'paymentMethods')->name('api.payment.methods');
