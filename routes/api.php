93b5fdc91e9b2dbb9afa56902d69d5f7e0772a8c
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-04-30 19:32:13 +0300

    update skills api issue

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\ApiController;
 Route::middleware('api.key')->prefix('api/v1')->group(function () {
     Route::controller(ApiController::class)->group(function () {
         Route::get('/about', 'aboutDev');
-        Route::get('skills', 'allSkills');
+        Route::get('/skills', 'allSkills');
         Route::get('/projects', 'allProjects');
         Route::post('/send/email', 'sendEmail');
         Route::get('/social/links', 'socialLinks');
