a526f86d99814a4436a6e34b3f885d52388b9c4e
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-05-02 12:19:12 +0300

    update send email

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('api.key')->prefix('v1')->group(function () {
     Route::controller(ApiController::class)->group(function () {
         Route::get('/about', 'aboutDev')->name('api.about');
         Route::get('/skills', 'allSkills')->name('api.skills');
         Route::get('/projects', 'allProjects')->name('api.projects');
+        Route::post('/send/email', 'sendEmail')->name('api.send.emails');
         Route::get('/social/links', 'socialLinks')->name('api.social.links');
         Route::get('/pricing/plans', 'PricingPlan')->name('api.pricing.plans');
         Route::get('/payment/methods', 'paymentMethods')->name('api.payment.methods');
@@ -15,5 +16,4 @@ Route::middleware('api.key')->prefix('v1')->group(function () {
     });
 });
 Route::controller(ApiController::class)->group(function () {
-    Route::post('/send/email', 'sendEmail')->name('api.send.emails');
 });
